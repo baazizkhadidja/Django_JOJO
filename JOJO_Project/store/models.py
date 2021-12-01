fa211d9fdcee5f3d5d32f2608a795afc99bd4079
Khadidja Baaziz <khadidjabaaziz@MacBook-Pro-de-Khadidja.local>
2021-12-01 14:19:53 +0100

    Add models and relations

--- JOJO_Project/store/models.py
+++ JOJO_Project/store/models.py
@@ -1,18 +1,54 @@
 from django.db import models
 
 # Create your models here.
-
-GATEAUX = {
-    'Gat1':{'name': 'Sablet'},
-    'Gat2':{'name':'Makroud'},
-    'Gat3':{'name':'Baklawa'},
-    'Gat4':{'name':'Croqué'},
-}
-
-ALBUMS = [
-    {'name':'Traditionnel', 'gateaux':[GATEAUX['Gat2'], GATEAUX['Gat3']]},
-    {'name':'Fete', 'gateaux':[GATEAUX['Gat1'],GATEAUX['Gat4']]},
-    {'name': 'Café','gateaux':[GATEAUX['Gat1'],GATEAUX['Gat2']]},
-]
+from django.db import models
 
 
+class Gateau(models.Model):
+    name = models.CharField('Nom du gateau', max_length=200, unique = False)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "gateau"
+
+
+
+class Album(models.Model):
+    reference = models.IntegerField("référence", null=True)
+    created_at = models.DateTimeField("date de création", auto_now_add=True)
+    available = models.BooleanField("disponible", default=True)
+    title = models.CharField('titre du disque ', max_length=200)
+    photo = models.FileField("URL dl image", upload_to="photo/")
+    gateaux = models.ManyToManyField(Gateau, related_name='albums', blank=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "album"
+
+
+class Contact(models.Model):
+    email = models.EmailField(max_length=100)
+    name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "contact"
+
+class Booking(models.Model):
+    created_at = models.DateTimeField("date d'envoie",auto_now_add=True)
+    contacted = models.BooleanField("demande traitée ?", default = False)
+    contact = models.ForeignKey(Contact, on_delete=models.CASCADE)
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    #album = models.OneToOneField(Album, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return str(self.contact)
+
+    class Meta:
+        verbose_name = "réservation"
--- JOJO_Project/store/urls.py
+++ JOJO_Project/store/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from store.views import listing
-# , detail, search, nouv_alb, voir_albums
+#from store.views import listing, detail, search
+# nouv_alb, voir_albums
 
 urlpatterns = [
-    path('listing/', listing,  name = "listing"),
-    #path('<int:album_id>', detail, name = "detail"),
+    #path('listing/', listing,  name = "listing"),
+    #path('<int:album_id>/', detail, name = "detail"),
     #path('search/', search, name = "search"),
     #path('form/', nouv_alb),
     #path('voir_albums/', voir_albums),
