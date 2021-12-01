bde3beec07d47c16f7e88664b5b679825c66974b
Khadidja Baaziz <khadidjabaaziz@MacBook-Pro-de-Khadidja.local>
2021-12-01 11:05:46 +0100

    first commit

---
+++ JOJO_Project/store/urls.py
@@ -0,0 +1,13 @@
+#from django.urls import path
+#from store.views import index
+# , listing, detail, search, nouv_alb, voir_albums
+
+urlpatterns = [
+    #path('index/', index, name = "index"),
+   # path('listing/', listing,  name = "listing"),
+    #path('<int:album_id>', detail, name = "detail"),
+    #path('search/', search, name = "search"),
+    #path('form/', nouv_alb),
+    #path('voir_albums/', voir_albums),
+
+]
